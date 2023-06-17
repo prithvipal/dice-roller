7a318336b7ba2a9b18cf0b398b9d5850112e472e
Prithvipal <prithvirathore99@gmail.com>
2023-06-18 00:12:15 +0530

    dice roller app code

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,31 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  final Color color1;
+  final Color color2;
+
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            color1,
+            color2,
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
